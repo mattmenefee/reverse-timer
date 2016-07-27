d87186d9bafc69b394ab7b11b09502e9d18384a8
Matt Menefee <matt.menefee@extractablemedia.com>
2016-07-27 10:42:28 -0400

    Initial XCode setup

---
+++ Reverse Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reverse Timer
+//
+//  Created by Matthew Menefee on 7/27/16.
+//  Copyright © 2016 SmartLogic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
