a0ae1ba2e9d593a8ef98513c2bfb8dfbb670016b
Matt Menefee <matt.menefee@extractablemedia.com>
2017-03-22 16:04:23 -0400

    Begin update to Swift v3

--- Reverse Timer/ViewController.swift
+++ Reverse Timer/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
     var counter:Int? = 0
-    var timer = NSTimer()
+    var timer = Timer()
     
     @IBOutlet weak var timeInput: UITextField!
     @IBOutlet weak var startButton: UIButton!
@@ -19,7 +19,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         timeInput.delegate = self
-        timeInput.addTarget(self, action: #selector(self.textFieldDidChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
+        timeInput.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: UIControlEvents.editingChanged)
 
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -29,15 +29,15 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func buttonWasPressed(sender: AnyObject) {
+    @IBAction func buttonWasPressed(_ sender: AnyObject) {
         counter = Int(timeInput.text!)!
         timer.invalidate() // just in case this button is tapped multiple times
         
         // start the timer
-        timer = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
     }
     
-    func textFieldDidChange(textField: UITextField) {
+    func textFieldDidChange(_ textField: UITextField) {
         //let hours = Int(time) / 3600
         //let minutes = Int(time) / 60 % 60
         //let seconds = Int(time) % 60
